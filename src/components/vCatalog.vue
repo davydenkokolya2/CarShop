6bbd530f51cd65d2bc6061e80a939edfcfadf2c1
Николай Давыденко <davydenko.kolya@bk.ru>
2021-09-29 23:22:20 +0300

    import vCatalog

--- src/components/vCatalog.vue
+++ src/components/vCatalog.vue
@@ -2,8 +2,12 @@
     
 </template>
 <script>
+import vCatalogItem from './vCatalogItem'
 export default {
     name: 'vCatalog',
+    components: {
+        vCatalogItem,
+    },
 }
 </script>
 <style scoped>
