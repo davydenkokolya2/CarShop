0ee55715329d202a49050d9329154b5059b463bc
Николай Давыденко <davydenko.kolya@bk.ru>
2021-09-29 23:07:48 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <vMainWrapper></vMainWrapper>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import vMainWrapper from './components/vMainWrapper'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    vMainWrapper
   }
 }
 </script>
---
+++ src/components/vMainWrapper.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="vMainWrapper">
+        <p>{{title}}</p>
+    </div>
+</template>
+
+<script>
+
+export default ({
+    name: "vMainWrapper",
+    components: {},
+    props: {},
+    data() {
+        return {
+            title: "Main wrapper"
+        }
+    },
+    computed: {},
+    methods: {},
+    watch: {},
+    mounted() {
+        console.log("Hello");
+    },
+})
+</script>
+
+<style>
+    .vMainWrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        max-width: 900px;
+        margin: 0 auto;
+    }
+</style>
+
